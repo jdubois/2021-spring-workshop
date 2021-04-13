b296763c5ff6a5d3bcea0ccb56fa016caeebdb13
Josh Long <josh@joshlong.com>
2021-04-13 02:03:43 -0700

    properties

--- app/native/run.sh
+++ app/native/run.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-docker run -e "SPRING_CONFIG_IMPORT=configserver:http://host.docker.internal:8888" docker.io/library/bootiful:0.0.1-SNAPSHOT
+docker run \
+  -e "spring.data.mongodb.host=host.docker.internal" \
+  -e "spring.config.import=configserver:http://host.docker.internal:8888" \
+  -e "eureka.client.serviceUrl.defaultZone=http://host.docker.internal:8761/eureka/" \
+  docker.io/library/bootiful:0.0.1-SNAPSHOT
