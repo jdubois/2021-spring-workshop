7a63bc4fd655f2f3134edb84113328e42e305685
Josh Long <josh@joshlong.com>
2021-04-13 00:46:03 -0700

    polish

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,60 @@
 package com.example.demo;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	private static final Log log = LogFactory.getLog(DemoApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(PersonRepository repository) {
+		return events -> Flux
+			.just("Julien", "Kylie", "Asir", "Theresa", "Rory", "Josh")
+			.map(name -> new Person(null, name, 0))
+			.flatMap(repository::save)
+			.subscribe();
+	}
+
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Person {
+
+	@Id
+	private String id;
+	private String name;
+	private int age;
+}
+
+interface PersonRepository extends ReactiveMongoRepository<Person, String> {
+}
+
+
+@RestController
+@RequiredArgsConstructor
+class PeopleRestController {
+
+	private final PersonRepository repository;
+
+	@GetMapping("/people")
+	Flux<Person> people() {
+		return this.repository.findAll();
 	}
 }
